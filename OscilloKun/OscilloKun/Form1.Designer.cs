b7523aa0fd5e05b77dd4b12fd3acf831de83afa7
woodrobo <woodrobo777@gmail.com>
2021-11-06 03:26:45 +0900

    ひとまず今のやつ

---
+++ OscilloKun/OscilloKun/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace OscilloKun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ConnectionButton = new System.Windows.Forms.Button();
+            this.MyTextBox = new System.Windows.Forms.TextBox();
+            this.PortComboBox = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.TriggercomboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TrigLevelNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SingleButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TimeComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CHComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrigLevelNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConnectionButton
+            // 
+            this.ConnectionButton.Location = new System.Drawing.Point(228, 15);
+            this.ConnectionButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ConnectionButton.Name = "ConnectionButton";
+            this.ConnectionButton.Size = new System.Drawing.Size(100, 29);
+            this.ConnectionButton.TabIndex = 0;
+            this.ConnectionButton.Text = "接続";
+            this.ConnectionButton.UseVisualStyleBackColor = true;
+            this.ConnectionButton.Click += new System.EventHandler(this.ConnectionButton_Click);
+            // 
+            // MyTextBox
+            // 
+            this.MyTextBox.Location = new System.Drawing.Point(16, 48);
+            this.MyTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.MyTextBox.Multiline = true;
+            this.MyTextBox.Name = "MyTextBox";
+            this.MyTextBox.Size = new System.Drawing.Size(311, 69);
+            this.MyTextBox.TabIndex = 1;
+            // 
+            // PortComboBox
+            // 
+            this.PortComboBox.FormattingEnabled = true;
+            this.PortComboBox.Location = new System.Drawing.Point(16, 15);
+            this.PortComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.PortComboBox.Name = "PortComboBox";
+            this.PortComboBox.Size = new System.Drawing.Size(160, 23);
+            this.PortComboBox.TabIndex = 2;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(95, 129);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1086, 419);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // TriggercomboBox
+            // 
+            this.TriggercomboBox.FormattingEnabled = true;
+            this.TriggercomboBox.Items.AddRange(new object[] {
+            "RISE",
+            "FALL"});
+            this.TriggercomboBox.Location = new System.Drawing.Point(461, 25);
+            this.TriggercomboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.TriggercomboBox.Name = "TriggercomboBox";
+            this.TriggercomboBox.Size = new System.Drawing.Size(160, 23);
+            this.TriggercomboBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Trigger";
+            // 
+            // TrigLevelNumericUpDown
+            // 
+            this.TrigLevelNumericUpDown.DecimalPlaces = 1;
+            this.TrigLevelNumericUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.TrigLevelNumericUpDown.Location = new System.Drawing.Point(461, 58);
+            this.TrigLevelNumericUpDown.Margin = new System.Windows.Forms.Padding(4);
+            this.TrigLevelNumericUpDown.Maximum = new decimal(new int[] {
+            33,
+            0,
+            0,
+            65536});
+            this.TrigLevelNumericUpDown.Name = "TrigLevelNumericUpDown";
+            this.TrigLevelNumericUpDown.Size = new System.Drawing.Size(160, 22);
+            this.TrigLevelNumericUpDown.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(399, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Level";
+            // 
+            // SingleButton
+            // 
+            this.SingleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SingleButton.Location = new System.Drawing.Point(1081, 89);
+            this.SingleButton.Margin = new System.Windows.Forms.Padding(4);
+            this.SingleButton.Name = "SingleButton";
+            this.SingleButton.Size = new System.Drawing.Size(100, 29);
+            this.SingleButton.TabIndex = 9;
+            this.SingleButton.Text = "SINGLE";
+            this.SingleButton.UseVisualStyleBackColor = true;
+            this.SingleButton.Click += new System.EventHandler(this.SingleButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(637, 30);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Time";
+            // 
+            // TimeComboBox
+            // 
+            this.TimeComboBox.FormattingEnabled = true;
+            this.TimeComboBox.Location = new System.Drawing.Point(699, 26);
+            this.TimeComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.TimeComboBox.Name = "TimeComboBox";
+            this.TimeComboBox.Size = new System.Drawing.Size(160, 23);
+            this.TimeComboBox.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(881, 29);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CH";
+            // 
+            // CHComboBox
+            // 
+            this.CHComboBox.FormattingEnabled = true;
+            this.CHComboBox.Location = new System.Drawing.Point(943, 25);
+            this.CHComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CHComboBox.Name = "CHComboBox";
+            this.CHComboBox.Size = new System.Drawing.Size(160, 23);
+            this.CHComboBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1292, 562);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CHComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TimeComboBox);
+            this.Controls.Add(this.SingleButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TrigLevelNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TriggercomboBox);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.PortComboBox);
+            this.Controls.Add(this.MyTextBox);
+            this.Controls.Add(this.ConnectionButton);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrigLevelNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectionButton;
+        private System.Windows.Forms.TextBox MyTextBox;
+        private System.Windows.Forms.ComboBox PortComboBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox TriggercomboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown TrigLevelNumericUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SingleButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox TimeComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CHComboBox;
+    }
+}
+
---
+++ OscilloKun/OscilloKun/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//UART
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace OscilloKun
+{
+    public partial class Form1 : Form
+    {
+        //serial
+        byte[] serial_buf = new byte[4096];
+        int serial_buf_counter = 0;
+        bool serial_buf_enable = true;
+        double sampling_us = 0.5;
+
+        //time
+        double[] interval_us = { 10, 20, 50, 100, 200, 500, 1000, 2000, 5000 };   //us
+        String[] interval_str = { "10us", "20us", "50us", "100us", "200us", "500us", "1ms", "2ms", "5ms" };
+
+        //CH
+        String[] CH_str = { "CH1", "CH1+CH2", "CH1+CH2+CH3", "CH1+CH2+CH3+CH4" };
+
+        public byte[] cobs_encode(byte[] data)
+        {
+            if (data.Length > 254)
+            {
+                Console.WriteLine("[error]data length must be less than 255");
+                byte[] tmp = new byte[0];
+                return tmp;
+            }
+            int len = data.Length + 2;
+            byte[] packet = new byte[len];
+
+            int zero_index = len - 1;
+            packet[len - 1] = 0;
+            for (int i = len - 2; i > 0; i--)
+            {
+                if (data[i - 1] == 0)
+                {
+                    packet[i] = (byte)(zero_index - i);
+                    zero_index = i;
+                }
+                else
+                {
+                    packet[i] = data[i - 1];
+                }
+            }
+            packet[0] = (byte)zero_index;
+
+            return packet;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                PortComboBox.Items.Add(port);
+            }
+            if (PortComboBox.Items.Count > 0)
+            {
+                PortComboBox.SelectedIndex = 0;
+            }
+
+            foreach (string str in interval_str)
+            {
+                TimeComboBox.Items.Add(str+"/div");
+            }
+            TimeComboBox.SelectedIndex = 0;
+
+            foreach (string str in CH_str)
+            {
+                CHComboBox.Items.Add(str);
+            }
+            CHComboBox.SelectedIndex = 0;
+
+            TriggercomboBox.SelectedIndex = 0;
+
+            // フォームをロードするときの処理
+            chart1.Series.Clear();  // ← 最初からSeriesが1つあるのでクリアします
+            chart1.ChartAreas.Clear();
+
+            // ChartにChartAreaを追加します
+            string chart_area1 = "Area1";
+            chart1.ChartAreas.Add(new ChartArea(chart_area1));
+            // ChartにSeriesを追加します
+            string legend1 = "CH1";
+            chart1.Series.Add(legend1);
+            // グラフの種別を指定
+            chart1.Series[legend1].ChartType = SeriesChartType.Line; // 折れ線グラフを指定してみます
+
+            // データを用意します
+            double[] y_values = new double[1000];
+            for (int i = 0; i < y_values.Length; i++)
+            {
+                y_values[i] = Math.Sin((double)i / 100.0 * 2.0 * Math.PI);
+            }
+
+            // データをシリーズにセットします
+            for (int i = 0; i < y_values.Length; i++)
+            {
+                chart1.Series[legend1].Points.AddY(y_values[i]);
+            }
+        }
+
+        private void ConnectionButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = 115200;
+            serialPort1.Parity = Parity.None;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Handshake = Handshake.None;
+            serialPort1.PortName = PortComboBox.SelectedItem.ToString();
+            serialPort1.Open();
+            MyTextBox.Text = serialPort1.PortName + "に接続しました。";
+        }
+
+        private void SingleButton_Click(object sender, EventArgs e)
+        {
+            //設定読み込み
+            string trig_mode = TriggercomboBox.SelectedItem.ToString();
+            double trig_level = (double)TrigLevelNumericUpDown.Value;
+
+            //オシロに送るコマンドはCOBS形式で
+            //1byte:START + Trigger
+            //2byte:サンプリング間隔100ns単位(上位8bit)
+            //3byte:サンプリング間隔100ns単位(下位8bit)
+            byte[] command = new byte[3] { 0x80, 0x00, 0x00 };
+
+            if (trig_mode == "RISE")
+            {
+                MyTextBox.Text = "Trigger:RISE";
+                command[0] |= 0x00;
+            }
+            else if(trig_mode == "FALL")
+            {
+                MyTextBox.Text = "Trigger:FALL";
+                command[0] |= 0x40;
+            }
+            else
+            {
+                MyTextBox.Text = "Select Trigger";
+            }
+
+            command[0] |= (byte)(trig_level * 10);
+
+            uint sampling_time = (uint)interval_us[TimeComboBox.SelectedIndex] / 10;  // /100 * 10
+            if(sampling_time < 5)
+            {
+                sampling_time = 5;
+            }
+            command[1] = (byte)((sampling_time >> 8) & 0xff);
+            command[2] = (byte)((sampling_time >> 0) & 0xff);
+
+            serial_buf_counter = 0;
+            serialPort1.Write(command, 0, 3);
+
+            sampling_us = sampling_time / 10;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] buf = new byte[serialPort1.BytesToRead];
+            serialPort1.Read(buf, 0, buf.Length);
+            if (serial_buf_enable)
+            {
+                for (int i = 0; i < buf.Length; i++)
+                {
+                    serial_buf[serial_buf_counter] = buf[i];
+                    serial_buf_counter++;
+                }
+
+                Console.WriteLine("counter:{0}", serial_buf_counter);
+
+                if (serial_buf_counter >= 4000)
+                {
+                    serial_buf_enable = false;
+                    Invoke(new DrawGraphDelegate(DrawGraph));
+                }
+            }
+
+        }
+
+       delegate void DrawGraphDelegate();
+       void DrawGraph()
+        {
+            MyTextBox.Text = DateTime.Now.ToString() + "\r\nDrawGraph\r\n";
+            MyTextBox.Text += "buf[0]:"+serial_buf[0].ToString();
+
+            //データを作成
+            double interval = interval_us[TimeComboBox.SelectedIndex];
+            double max = interval * 10;
+            int data_length = (int)(max / sampling_us);
+            if(data_length > 1000)
+            {
+                data_length = 1000;
+            }
+
+            double[] time = new double[data_length];
+            double[] ch1_vol = new double[data_length];
+            double[] ch2_vol = new double[data_length];
+            double[] ch3_vol = new double[data_length];
+            double[] ch4_vol = new double[data_length];
+            for (int i = 0; i < data_length; i++)
+            {
+                ch1_vol[i] = (double)serial_buf[i] / 256.0 * 3.3;
+                ch2_vol[i] = (double)serial_buf[i+1000] / 256.0 * 3.3;
+                ch3_vol[i] = (double)serial_buf[i+2000] / 256.0 * 3.3;
+                ch4_vol[i] = (double)serial_buf[i+3000] / 256.0 * 3.3;
+                time[i] = sampling_us * i;
+            }
+
+            // フォームをロードするときの処理
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            // ChartにChartAreaを追加
+            string chart_area = "OscilloArea";
+            chart1.ChartAreas.Add(new ChartArea(chart_area));
+            chart1.ChartAreas[0].AxisX.Title = "Time(us)";
+            chart1.ChartAreas[0].AxisY.Title = "Voltage(V)";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = max;
+            chart1.ChartAreas[0].AxisX.Interval = interval;
+            // ChartにSeriesを追加
+            int ch_num = CHComboBox.SelectedIndex + 1;
+            string legend1 = "CH1";
+            string legend2 = "CH2";
+            string legend3 = "CH3";
+            string legend4 = "CH4";
+            if (ch_num >= 1)
+            {
+                chart1.Series.Add(legend1);
+                chart1.Series[legend1].ChartType = SeriesChartType.Line;
+                for (int i = 0; i < data_length; i++)
+                {
+                    chart1.Series[legend1].Points.AddXY(time[i], ch1_vol[i]);
+                }
+            }
+            if (ch_num >= 2)
+            {
+                chart1.Series.Add(legend2);
+                chart1.Series[legend2].ChartType = SeriesChartType.Line;
+                for (int i = 0; i < data_length; i++)
+                {
+                    chart1.Series[legend2].Points.AddXY(time[i], ch2_vol[i]);
+                }
+            }
+            if (ch_num >= 3)
+            {
+                chart1.Series.Add(legend3);
+                chart1.Series[legend3].ChartType = SeriesChartType.Line;
+                for (int i = 0; i < data_length; i++)
+                {
+                    chart1.Series[legend3].Points.AddXY(time[i], ch3_vol[i]);
+                }
+            }
+            if (ch_num >= 4)
+            {
+                chart1.Series.Add(legend4);
+                chart1.Series[legend4].ChartType = SeriesChartType.Line;
+                for (int i = 0; i < data_length; i++)
+                {
+                    chart1.Series[legend4].Points.AddXY(time[i], ch4_vol[i]);
+                }
+            }
+
+            serial_buf_counter = 0;
+            serial_buf_enable = true;
+        }
+    }
+}
